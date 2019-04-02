7684fa21de232da602d89c3ca52c756b4f2768d4
white <2916144319@qq.com>
2019-04-02 16:34:06 +0800

    初始建立

---
+++ src/main/java/com/hl7/get/MessageGet.java
@@ -0,0 +1,23 @@
+package com.hl7.get;
+
+public class MessageGet{
+
+    /**
+     * 作为服务器将获取各种HL7协议相关数据包
+     * 
+     */
+
+
+    public MessageGet(){
+
+    }
+
+    public MessageGet(int port){
+        
+    }
+
+    void start(){
+        EventLoopGroup boss = new NioEventLoopGroup();
+        
+    }
+}
